159d6097e65613bffd1fc520275466c8f24aac76
jassonix <jossanix@gmail.com>
2026-04-24 11:13:41 +0300

    Update SteamProfileDto.java

--- src/main/java/com/example/demo/SteamProfileDto.java
+++ src/main/java/com/example/demo/SteamProfileDto.java
@@ -2,7 +2,6 @@ package com.example.demo;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-// Аннотация игнорирует лишние поля из JSON, которых нет в этом классе
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record SteamProfileDto(
         String nickname,
